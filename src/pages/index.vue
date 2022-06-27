753339a38c321691736f02b87590f22e1d59d0a5
Dagim Shimelis <dagimshimelis1992@gmail.com>
2022-06-27 08:36:39 +0300

    🧱 clean boilerplate
    - created a clean boiler plate for vite/tailwind/other modules
    - created empty placeholder folders

---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Hello world</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
+<route lang="yaml">
+meta:
+    layout: empty
+</route>
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages';
+import Layouts from 'vite-plugin-vue-layouts';
+import Components from 'vite-plugin-components'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), Pages(),Components(), Layouts({layoutsDirs: 'src/layouts'})],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  }, 
+  test: {
+    globals: true
+  }
+})
